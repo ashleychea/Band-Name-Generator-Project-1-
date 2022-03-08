95fc272d87c6e577524bdb7098e4f041d88b2c10
Ashley Chea <60674422+ashleychea@users.noreply.github.com>
2022-03-08 14:47:33 +0000

    *Changed "How many pets do you have" to "What is the name of your pet" so that the flow of what I wanted to be outputted sounded better.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ print("Hello and Welcome to Band Generator")
 #2. Ask the user for the city that they grew up in.
 city = input("What's the name of the city you grew up in?\n")
 #3. Ask the user for the name of a pet.
-pet = input("How many pets do you have?\n")
+pet = input("What is the name of pet?\n")
 #4. Combine the name of their city and pet and show them their band name.
 print("Your new band name is " + city + " " + pet)
 #5. Make sure the input cursor shows on a new line, see the example at:
